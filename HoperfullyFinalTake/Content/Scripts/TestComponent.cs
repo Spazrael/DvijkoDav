34ad1be8ce9956ef18242863417bbe9ff99b2ea2
Spazrael <daniil127@mail.ru>
2024-01-24 22:45:17 +0300

    GAVNISHKO

---
+++ HoperfullyFinalTake/Content/Scripts/TestComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Engine;
+using Engine.BaseAssets.Components;
+
+namespace HoperfullyFinalTake.Content.Scripts
+{
+    public class TestComponent : BehaviourComponent
+    {
+        [SerializedField]
+        private double Value = 5;
+        public override void Start()
+        {
+            Logger.Log(LogType.Info, $"Start {Value}");
+        }
+        public override void Update()
+        {
+            
+        }
+    }
+}
